8938983e2a0979fd38e0483de7c31f1292720156
Jobe Range <rangejobe@gmail.com>
2018-06-21 23:48:58 -0700

    last push

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,12 +1,19 @@
 
+// setting variables for breaking up html into blocks to appear at seperate times
+
 var page1 = $(".page1")
 var page2 = $(".page2")
 var page3 = $(".page3")
+
+// on start hiding two "pages" and displaying first page
 
 $(document).ready(function() {
     page2.hide();
     page3.hide();
 })
+
+//when clicking start game need to display second page and start timer
+// when timer finishes it will display last page with score and replay button
 
 $(".start").click(function timer() {
     page2.show();
@@ -29,6 +36,9 @@ $(".start").click(function timer() {
       }
 });
 
+//when game is over need to restart timer and display page 2 again
+
+
 $(".replay").click(function timer() {
     page2.show();
     page1.hide();
@@ -50,3 +60,10 @@ $(".replay").click(function timer() {
       }
 });
 
+// select and store answers
+// limit answers given
+// value answers as ture or false
+// record and display flase or true amount
+// check for unanswered questions
+// terminate game when all answered
+// 
